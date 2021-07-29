b052766b1bde10f645b6d2c6f2fd3a8c73820343
Ken Otuo <mrkoajr@gmail.com>
2021-07-29 17:45:24 -0400

    updated models file, also seeds file

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-// import models
+/*// import models
 const Product = require('./Product');
 const Category = require('./category');
 const Tag = require('./tag');
@@ -41,5 +41,27 @@ module.exports = {
   Category,
   Tag,
   ProductTag,
-};
+}; */
+
+// import models 
+const Product = require('./Product'); 
+const Category = require('./category'); 
+const Tag = require('./tag'); 
+const ProductTag = require('./productTag');
+
+//Products.belongsTo.Category 
+Product.belongsTo(Category, { foreignKey: "category_id", 
+// onDelete: "CASCADE", as: "product_category" });
+
+// Categories have many Products 
+Category.hasMany(Product, { foreignKey: "category_id", 
+// onDelete: "CASCADE", as: "product_category" });
+
+// Products belongToMany Tags (through ProductTag) 
+Product.belongsToMany(Tag, { through: { model: ProductTag, unique: false }, as: "tags" });
+
+// Tags belongToMany Products (through ProductTag) 
+Tag.belongsToMany(Product, { through: { model: ProductTag, unique: false }, as: "products" });
+
+module.exports = { Product, Category, Tag, ProductTag, };
 
